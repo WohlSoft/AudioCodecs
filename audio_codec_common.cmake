aa04266b3e24b44f0d9f59f4ed6307981d527fd1
Wohlstand <admin@wohlnet.ru>
2017-08-02 11:51:16 +0300

    Common compiler flags for all libraries

---
+++ audio_codec_common.cmake
@@ -0,0 +1,26 @@
+# Strip garbage
+if(APPLE)
+    set(LINK_FLAGS_RELEASE  "${LINK_FLAGS_RELEASE} -dead_strip")
+ELSEIF(NOT MSVC)
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -Os -s -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,-s")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Os -s -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,-s")
+    set(LINK_FLAGS_RELEASE  "${LINK_FLAGS_RELEASE} -Wl,--gc-sections -Wl,-s")
+ENDIF()
+
+# Global optimization flags
+IF(NOT MSVC)
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -fno-omit-frame-pointer -")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fno-omit-frame-pointer")
+ENDIF()
+
+if (CMAKE_BUILD_TYPE EQUAL "RELEASE")
+    add_definitions(-DNDEBUG)
+ENDIF()
+
+# -fPIC thing
+IF(NOT MSVC)
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -fPIC")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fPIC")
+ENDIF()
+
+
