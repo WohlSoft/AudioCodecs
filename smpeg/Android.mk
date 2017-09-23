04a0d098b17306472cdb0c1d3220e91f461edc43
Wohlstand <admin@wohlnet.ru>
2017-09-23 19:49:25 +0300

    SMPEG: Nuke Autotools because of CMake and QMake are in use

--- smpeg/Android.mk
+++ smpeg/Android.mk
@@ -8,32 +8,32 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_CFLAGS := -DSTDC_HEADERS=1 -DHAVE_SYS_TYPES_H=1 -DHAVE_SYS_STAT_H=1 -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_MEMORY_H=1 -DHAVE_STRINGS_H=1 -DHAVE_INTTYPES_H=1 -DHAVE_STDINT_H=1 -DHAVE_UNISTD_H=1 -DHAVE_DLFCN_H=1 -D_THREAD_SAFE -DTHREADED_AUDIO -DNOCONTROLS
 
 LOCAL_SRC_FILES := \
-    smpeg.cpp \
-    MPEG.cpp \
-    MPEGlist.cpp \
-    MPEGring.cpp \
-    MPEGstream.cpp \
-    MPEGsystem.cpp \
-    audio/MPEGaudio.cpp \
-    audio/bitwindow.cpp \
-    audio/filter.cpp \
-    audio/filter_2.cpp \
-    audio/hufftable.cpp \
-    audio/mpeglayer1.cpp \
-    audio/mpeglayer2.cpp \
-    audio/mpeglayer3.cpp \
-    audio/mpegtable.cpp \
-    audio/mpegtoraw.cpp \
-    video/MPEGvideo.cpp \
-    video/decoders.cpp \
-    video/floatdct.cpp \
-    video/gdith.cpp \
-    video/jrevdct.cpp \
-    video/motionvec.cpp \
-    video/parseblock.cpp \
-    video/readfile.cpp \
-    video/util.cpp \
-    video/video.cpp
+    src/smpeg.cpp \
+    src/MPEG.cpp \
+    src/MPEGlist.cpp \
+    src/MPEGring.cpp \
+    src/MPEGstream.cpp \
+    src/MPEGsystem.cpp \
+    src/audio/MPEGaudio.cpp \
+    src/audio/bitwindow.cpp \
+    src/audio/filter.cpp \
+    src/audio/filter_2.cpp \
+    src/audio/hufftable.cpp \
+    src/audio/mpeglayer1.cpp \
+    src/audio/mpeglayer2.cpp \
+    src/audio/mpeglayer3.cpp \
+    src/audio/mpegtable.cpp \
+    src/audio/mpegtoraw.cpp \
+    src/video/MPEGvideo.cpp \
+    src/video/decoders.cpp \
+    src/video/floatdct.cpp \
+    src/video/gdith.cpp \
+    src/video/jrevdct.cpp \
+    src/video/motionvec.cpp \
+    src/video/parseblock.cpp \
+    src/video/readfile.cpp \
+    src/video/util.cpp \
+    src/video/video.cpp
 
 LOCAL_LDLIBS :=
 LOCAL_STATIC_LIBRARIES :=
