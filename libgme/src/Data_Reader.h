7f17dbbff4a7583d81824cbfc129e73d3c3387ed
Vitaly Novichkov <admin@wohlnet.ru>
2018-01-27 21:22:20 +0300

    GME: Apply final version after official adding of ZLib in-memory support https://bitbucket.org/mpyne/game-music-emu/pull-requests/5/added-zlib-support-for-memory-reading/https://bitbucket.org/mpyne/game-music-emu/pull-requests/5/added-zlib-support-for-memory-reading/

--- libgme/src/Data_Reader.h
+++ libgme/src/Data_Reader.h
@@ -8,7 +8,6 @@
 
 #ifdef HAVE_ZLIB_H
 #include <zlib.h>
-#include <vector>
 #endif
 
 // Supports reading and finding out how many bytes are remaining
@@ -70,10 +69,9 @@ public:
 	long tell() const;
 	blargg_err_t seek( long );
 private:
-	void* file_;
+	void* file_; // Either FILE* or zlib's gzFile
 #ifdef HAVE_ZLIB_H
-	gzFile gzfile_;
-	long size_;
+	long size_; // TODO: Fix ABI compat
 #endif /* HAVE_ZLIB_H */
 };
 
@@ -81,6 +79,9 @@ private:
 class Mem_File_Reader : public File_Reader {
 public:
 	Mem_File_Reader( const void*, long size );
+#ifdef HAVE_ZLIB_H
+	~Mem_File_Reader( );
+#endif /* HAVE_ZLIB_H */
 
 public:
 	long size() const;
@@ -90,12 +91,14 @@ public:
 private:
 #ifdef HAVE_ZLIB_H
 	bool gz_decompress();
-	std::vector<char> m_raw_data;
 #endif /* HAVE_ZLIB_H */
 
 	const char* m_begin;
 	long m_size;
 	long m_pos;
+#ifdef HAVE_ZLIB_H
+	bool m_ownedPtr = false; // set if we must free m_begin
+#endif /* HAVE_ZLIB_H */
 };
 
 
