3a7329425954393900d8d75e6195034e8cd7f8e4
Vitaly Novichkov <admin@wohlnet.ru>
2018-11-09 04:23:47 +0300

    Attempt to fix libModPlug build on macOS

--- libmodplug/src/config.h
+++ libmodplug/src/config.h
@@ -12,15 +12,6 @@
 /* Define to 1 if you have the <dlfcn.h> header file. */
 /* #undef HAVE_DLFCN_H */
 
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the <malloc.h> header file. */
-#define HAVE_MALLOC_H 1
-
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
 /* Define to 1 if you have the `setenv' function. */
 #if !defined(_WIN32) && !defined(HAVE_CMAKE_CONFIG_H)
 #define HAVE_SETENV 1
