662bf6aae180121dd0f75c62cb0b753de0525b42
Vitaly Novichkov <admin@wohlnet.ru>
2019-12-01 15:45:44 +0300

    Added Sezero's fork of Tremor

---
+++ tremor/cmake/config.h
@@ -0,0 +1,10 @@
+/* Define to 1 if you have <alloca.h> and it should be used (not on Ultrix).
+   */
+#ifndef _WIN32
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+#define VAR_ARRAYS 1
+#endif
+
+#define HAVE_MEMORY_H 1
+
