67bb1db9c5ae871a0d032d03f1b7198303589153
Wohlstand <admin@wohlnet.ru>
2020-11-27 00:15:30 +0300

    Added local copy of SDL2 stable release
    Added an option to build the SDL2 copy from the local repo here. Also, remove some project folders which making the big trouble on Windows while building.

---
+++ _common/update-sdl2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget --content-disposition https://hg.libsdl.org/SDL/archive/release-2.0.12.tar.gz -O SDL-repo.tar.gz
+tar -xf SDL-repo.tar.gz
+mv SDL-release-2.0.12 SDL2
+rm -Rf SDL2/VisualC/
+rm -Rf SDL2/VisualC-WinRT/
+rm -Rf SDL2/visualtest/
+rm -Rf SDL2/test/
+rm -Rf SDL2/Xcode/
+rm -Rf SDL2/Xcode-iOS/
+rm -Rf SDL2/android-project/
+rm -Rf SDL2/android-project-ant/
+
+rm -Rf ../SDL2
+mv SDL2 ../SDL2
+rm SDL-repo.tar.gz
--- download_sdl2_hg.cmake
+++ download_sdl2_hg.cmake
@@ -34,7 +34,9 @@ set(SDL2_GIT_BRANCH "origin/master" CACHE STRING "GIT branch for SDL2 (unofficia
 # Remove this workaround when the tarball is symlink-free for better Windows compatibility.
 # In the meantime, use the auto-tracking SDL2 Git repo:
 set(SDL2_PROJECT_BRANCH)
-if(WIN32 AND MINGW)
+if(USE_LOCAL_SDL2)
+    message("== SDL2 will be built from a local copy")
+elseif(WIN32 AND MINGW)
     set(SDL_SOURCE_PATH_GIT "https://github.com/spurious/SDL-mirror.git")
     message("== SDL2 will be downloaded as unofficial GIT repository from '${SDL2_GIT_BRANCH}' revision")
     set(SDL2_PROJECT_BRANCH GIT_TAG "${SDL2_GIT_BRANCH}")
@@ -48,24 +50,45 @@ if(APPLE)
     set(APPLE_FLAGS "-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
 endif()
 
-ExternalProject_Add(
-    SDL2HG
-    PREFIX ${CMAKE_BINARY_DIR}/external/SDL2
-    GIT_REPOSITORY ${SDL_SOURCE_PATH_GIT}
-    ${SDL2_PROJECT_BRANCH}
-    URL ${SDL_SOURCE_PATH_URL}
-    CMAKE_ARGS
-        "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-        "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
-        "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}"
-        -DSNDIO=OFF
-        -DSDL_SHARED=${BUILD_SDL2_SHARED}
-        -DSDL_STATIC=${BUILD_SDL2_STATIC}
-        -DCMAKE_DEBUG_POSTFIX=${CMAKE_DEBUG_POSTFIX}
-        ${SDL2_CMAKE_FPIC_FLAG}
-        ${SDL2_WASAPI_FLAG}
-        ${APPLE_FLAGS}
-)
+if(USE_LOCAL_SDL2)
+    ExternalProject_Add(
+        SDL2HG
+        PREFIX ${CMAKE_BINARY_DIR}/external/SDL2
+        DOWNLOAD_COMMAND ""
+        SOURCE_DIR ${CMAKE_SOURCE_DIR}/SDL2/
+        CMAKE_ARGS
+            "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+            "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+            "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}"
+            -DSNDIO=OFF
+            -DSDL_SHARED=${BUILD_SDL2_SHARED}
+            -DSDL_STATIC=${BUILD_SDL2_STATIC}
+            -DCMAKE_DEBUG_POSTFIX=${CMAKE_DEBUG_POSTFIX}
+            ${SDL2_CMAKE_FPIC_FLAG}
+            ${SDL2_WASAPI_FLAG}
+            ${APPLE_FLAGS}
+    )
+else()
+    ExternalProject_Add(
+        SDL2HG
+        PREFIX ${CMAKE_BINARY_DIR}/external/SDL2
+        GIT_REPOSITORY ${SDL_SOURCE_PATH_GIT}
+        ${SDL2_PROJECT_BRANCH}
+        URL ${SDL_SOURCE_PATH_URL}
+        CMAKE_ARGS
+            "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+            "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+            "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}"
+            -DSNDIO=OFF
+            -DSDL_SHARED=${BUILD_SDL2_SHARED}
+            -DSDL_STATIC=${BUILD_SDL2_STATIC}
+            -DCMAKE_DEBUG_POSTFIX=${CMAKE_DEBUG_POSTFIX}
+            ${SDL2_CMAKE_FPIC_FLAG}
+            ${SDL2_WASAPI_FLAG}
+            ${APPLE_FLAGS}
+    )
+endif()
+
 
 # Install built SDL's headers and libraries into actual installation directory
 install(
