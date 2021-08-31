0a905de9f2c59b63a5323ee0b7b172d86aa7483a
Wohlstand <admin@wohlnet.ru>
2021-08-31 13:51:49 +0300

    Updated SDL2 into 2.0.16

--- _common/update-sdl2.sh
+++ _common/update-sdl2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget --content-disposition https://github.com/libsdl-org/SDL/archive/release-2.0.14.tar.gz -O SDL-repo.tar.gz
+wget --content-disposition https://github.com/libsdl-org/SDL/archive/release-2.0.16.tar.gz -O SDL-repo.tar.gz
 tar -xf SDL-repo.tar.gz
-mv SDL-release-2.0.14 SDL2
+mv SDL-release-2.0.16 SDL2
 rm -Rf SDL2/VisualC/
 rm -Rf SDL2/VisualC-WinRT/
 rm -Rf SDL2/visualtest/
