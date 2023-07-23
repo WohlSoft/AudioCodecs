798b2bb1a8d8373d2d77bdd9288855cfec7675cc
Wohlstand <admin@wohlnet.ru>
2023-07-23 05:12:46 +0300

    Misc: Put GME update code into separated script

---
+++ _common/update-gme.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function updateGME()
+{
+    printf "\n=== Update $2 library... ===\n\n"
+
+    if [ ! -d "../$2" ]; then
+        echo "ERROR! Folder $2 is not exists!"
+        echo "-- Did you ran the script not from the _common folder? --"
+        echo "-- Please change into _common folder and run this script again! --"
+        exit 1
+    fi
+
+    echo "Removing old $2 folder..."
+    rm -Rf "../$2/src/"*.cpp
+    rm -Rf "../$2/src/"*.h
+    rm -Rf "../$2/src/ext/"*.c
+    rm -Rf "../$2/src/ext/"*.h
+
+    git clone --depth 1 "$1" "../$2-tmp"
+
+    echo "Copyng necessary files.."
+    cp "../$2-tmp/gme/"*.cpp "../$2/src/"
+    cp "../$2-tmp/gme/"*.h "../$2/src/"
+    cp "../$2-tmp/gme/ext/"*.c "../$2/src/ext/"
+    cp "../$2-tmp/gme/ext/"*.h "../$2/src/ext/"
+
+    echo "Removing unnecessary $2-tmp folder..."
+    rm -Rf "../$2-tmp"
+}
+
+updateGME https://Wohlstand@bitbucket.org/mpyne/game-music-emu.git libgme
+#updateGME https://Wohlstand@bitbucket.org/Wohlstand/game-music-emu.git libgme
+
+printf "\n\nAll libraries has been updated!\n\n"
+
+exit 0
--- _common/update-libs.sh
+++ _common/update-libs.sh
@@ -19,35 +19,6 @@ function updateRepo()
     rm -Rf "../$2/.git"
 }
 
-function updateGME()
-{
-    printf "\n=== Update $2 library... ===\n\n"
-
-    if [ ! -d "../$2" ]; then
-        echo "ERROR! Folder $2 is not exists!"
-        echo "-- Did you ran the script not from the _common folder? --"
-        echo "-- Please change into _common folder and run this script again! --"
-        exit 1
-    fi
-
-    echo "Removing old $2 folder..."
-    rm -Rf "../$2/src/"*.cpp
-    rm -Rf "../$2/src/"*.h
-    rm -Rf "../$2/src/ext/"*.c
-    rm -Rf "../$2/src/ext/"*.h
-
-    git clone --depth 1 "$1" "../$2-tmp"
-
-    echo "Copyng necessary files.."
-    cp "../$2-tmp/gme/"*.cpp "../$2/src/"
-    cp "../$2-tmp/gme/"*.h "../$2/src/"
-    cp "../$2-tmp/gme/ext/"*.c "../$2/src/ext/"
-    cp "../$2-tmp/gme/ext/"*.h "../$2/src/ext/"
-
-    echo "Removing unnecessary $2-tmp folder..."
-    rm -Rf "../$2-tmp"
-}
-
 function updateXMP()
 {
     printf "\n=== Update $2 library... ===\n\n"
@@ -83,10 +54,7 @@ function updateXMP()
 updateRepo https://github.com/Wohlstand/libADLMIDI.git libADLMIDI
 updateRepo https://github.com/Wohlstand/libOPNMIDI.git libOPNMIDI
 updateRepo https://github.com/Wohlstand/libEDMIDI.git libEDMIDI
-updateGME https://Wohlstand@bitbucket.org/mpyne/game-music-emu.git libgme
-#updateGME https://Wohlstand@bitbucket.org/Wohlstand/game-music-emu.git libgme
 updateXMP https://github.com/libxmp/libxmp.git libxmp
-
 
 
 printf "\n\nAll libraries has been updated!\n\n"
