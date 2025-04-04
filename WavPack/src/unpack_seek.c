14ee7eb249634c9a50ae6bd1dfca02cbe108158b
Wohlstand <admin@wohlnet.ru>
2025-04-04 20:56:29 +0300

    WavPack: Fixed small warning
    ```
    src/unpack_seek.c: In function ‘find_header’:
    src/unpack_seek.c:306:59: warning: pointer ‘ep’ used after ‘free’ [-Wuse-after-free]
      306 |                         return reader->get_pos (id) - (ep - sp + 4);
          |                                                        ~~~^~~~
    src/unpack_seek.c:305:25: note: call to ‘free’ here
      305 |                         free (buffer);
    ```

--- WavPack/src/unpack_seek.c
+++ WavPack/src/unpack_seek.c
@@ -302,8 +302,9 @@ static int64_t find_header (WavpackStreamReader64 *reader, void *id, int64_t fil
                     WavpackLittleEndianToNative (wphdr, WavpackHeaderFormat);
 
                     if (wphdr->block_samples && (wphdr->flags & INITIAL_BLOCK)) {
+                        int len = ep - sp;
                         free (buffer);
-                        return reader->get_pos (id) - (ep - sp + 4);
+                        return reader->get_pos (id) - (len + 4);
                     }
 
                     if (wphdr->ckSize > 1024)
